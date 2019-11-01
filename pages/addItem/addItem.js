3bd8ace53668fefc2b6e5aef86b5838894bfa033
jinjin.dong <jinjin.dong@11bee.com>
2019-11-02 00:18:19 +0800

    日期的选择

--- pages/addItem/addItem.js
+++ pages/addItem/addItem.js
@@ -2,17 +2,40 @@ const app = getApp();
 
 Page({
     data: {
-        date: new Date().toLocaleString(),
+        today: new Date().toLocaleDateString(),
+        date: new Date().toLocaleDateString(),
+        changed: false,
         label: '',
         value: ''
     },
-    submit: function () {
-        console.log('提交')
-    },
     bindDateChange: function(e) {
-        console.log('picker发送选择改变，携带值为', e.detail.value)
+        console.log('picker发送选择改变，携带值为', e.detail.value);
         this.setData({
-            date: e.detail.value
+            date: e.detail.value,
+            changed: true
         })
+    },
+    labelInput: function (e) {
+        this.setData({
+            label: e.detail.value
+        })
+    },
+    moneyInput: function (e) {
+        this.setData({
+            value: e.detail.value
+        })
+    },
+    submit: function () {
+        console.log('提交');
+        const dateArr = (this.data.date).split('-');
+        const year = dateArr[0];
+        const month = dateArr[1];
+        const day = dateArr[2];
+        const newData = {
+            label: this.data.label,
+            money: this.data.value,
+            day: `${month}/${day}`
+        };
+        console.log(newData);
     },
 });
